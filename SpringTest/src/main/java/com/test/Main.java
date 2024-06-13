b6dc4b5f073b519feca59f80b68df9ee01e94ecd
zhangzeyu <v_zhangzeyu.js@chinatelecom.cn>
2024-06-13 08:26:49 +0800

    测试提交

--- SpringTest/src/main/java/com/test/Main.java
+++ SpringTest/src/main/java/com/test/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("test.xml");
     }
 }
